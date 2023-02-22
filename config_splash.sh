46aee4e86741a6cf393e86343169d2258d1c3e55
Rosso2004 <simone.rosso004@gmail.com>
2023-02-22 21:35:39 +0100

    Config Splash

---
+++ config_splash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Aggiornamento dei pacchetti di sistema
+sudo apt-get update && sudo apt-get upgrade
+
+# Installazione di Plymouth e dei suoi temi
+sudo apt-get install plymouth -y
+sudo apt-get install plymouth-themes -y
+
+# Installazione di RPD-Plym-Splash per abilitare/disabilitare lo splash screen dal raspi-config
+sudo apt -y install rpd-plym-splash
+
+# Installazione di Git
+sudo apt-get install git -y
+
+# Clonazione del repository dei temi di Plymouth e impostazione del tema di default
+sudo rm -f /usr/share/plymouth/themes/spinner/background-tile.png
+sudo mv background-tile.png /usr/share/plymouth/themes/spinner
+sudo plymouth-set-default-theme -R spinner
+sudo reboot
