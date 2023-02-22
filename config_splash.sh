68576ae136d69ae8f77051b72e55bae49a40ed74
Rosso2004 <simone.rosso004@gmail.com>
2023-02-22 22:20:03 +0100

    Update config_splash.sh

--- config_splash.sh
+++ config_splash.sh
@@ -1,60 +1,24 @@
-function progress {
-    echo -ne "\r\e[K"
-    local w=80 p=$1;  shift
-    printf -v progressbar "%0.s#" $(seq 1 $(($p*$w/100)))
-    printf -v spaces "%0.s " $(seq $((($p*$w/100)+1)) $w)
-    printf "\r\e[K|%s%s| %3d %% %s" "$progressbar" "$spaces" "$p" "$*";
-}
+#!/bin/bash
 
-echo -n "Aggiornare il sistema? [s/N]: "
-read update
-if [ "$update" = "s" ] || [ "$update" = "S" ]; then
-    echo "Aggiornamento del sistema in corso..."
-    sudo apt-get update && sudo apt-get upgrade &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Aggiornamento del sistema non eseguito."
-        ERROR=1
-    else
-        echo "Aggiornamento del sistema eseguito con successo."
-    fi
-fi
+read -p "Desideri eseguire l'aggiornamento del sistema? [Y/n]" choice
+case "$choice" in 
+  y|Y ) 
+    echo "Aggiornamento dei pacchetti di sistema..."
+    sudo apt-get update && sudo apt-get upgrade
+    ;;
+  n|N )
+    echo "Aggiornamento del sistema non eseguito."
+    ;;
+  * ) echo "Scelta non valida. L'aggiornamento del sistema non verrà eseguito."
+      ;;
+esac
 
-echo "Installazione di Plymouth e dei suoi temi..."
-sudo apt-get install plymouth -y &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Installazione di Plymouth non riuscita."
-    ERROR=1
-else
-    progress 25
-    echo
-fi
+sudo apt-get install plymouth -y
+sudo apt-get install plymouth-themes -y
 
-echo "Installazione di RPD-Plym-Splash per abilitare/disabilitare lo splash screen dal raspi-config..."
-sudo apt -y install rpd-plym-splash &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Installazione di RPD-Plym-Splash non riuscita."
-    ERROR=1
-else
-    progress 50
-    echo
-fi
+sudo apt -y install rpd-plym-splash
 
-echo "Clonazione del repository dei temi di Plymouth e impostazione del tema di default..."
-sudo rm -f /usr/share/plymouth/themes/spinner/background-tile.png &> /dev/null
-sudo mv background-tile.png /usr/share/plymouth/themes/spinner &> /dev/null
-sudo plymouth-set-default-theme -R spinner &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Clonazione del repository dei temi di Plymouth non riuscita."
-    ERROR=1
-else
-    progress 75
-    echo
-fi
-
-echo -ne "\r\e[K"
-progress 100
-echo
-
-if [ $ERROR -eq 0 ]; then
-    echo "Eseguito con successo!"
-fi
+sudo rm -f /usr/share/plymouth/themes/spinner/background-tile.png
+sudo mv background-tile.png /usr/share/plymouth/themes/spinner
+sudo plymouth-set-default-theme -R spinner
+sudo reboot
