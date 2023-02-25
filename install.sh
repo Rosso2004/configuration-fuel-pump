2c2bf76ce44be4a6b52002aa04a5765814d8c5d1
Rosso2004 <simone.rosso004@gmail.com>
2023-02-25 16:46:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,19 +7,23 @@ options=(1 "Install the custom bootloader"
          2 "Configure the kiosk on startup"
          3 "Reboot the system appol")
 
-option=$(whiptail --title "Pump Configuration" --menu "Choose an option:" 15 60 4 "${options[@]}" 3>&1 1>&2 2>&3)
+while true; do
+    option=$(whiptail --title "Pump Configuration" --menu "Choose an option:" 15 60 4 "${options[@]}" 3>&1 1>&2 2>&3)
 
-if [ $? -eq 0 ]; then
-    case $option in
-        1)
-            sh .config/config_splash.sh
-            ;;
-        2)
+    if [ $? -eq 0 ]; then
+        case $option in
+            1)
+                sh .config/config_splash.sh
+                ;;
+            2)
 
-            sh .config/config_kiosk.sh
-            ;;
-        3)
-            sudo reboot
-            ;;
-    esac
-fi
+                sh .config/config_kiosk.sh
+                ;;
+            3)
+                sudo reboot
+                ;;
+        esac
+    else
+        break
+    fi
+done
