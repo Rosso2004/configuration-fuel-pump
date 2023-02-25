4f7f818991554aa0a13b1baf660f280e6b814741
Rosso2004 <simone.rosso004@gmail.com>
2023-02-25 16:40:09 +0100

    Install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo chmod +x .config/config_splash.sh
+sudo chmod +x .config/config_kiosk.sh
+
+options=(1 "Install the custom bootloader"
+         2 "Configure the kiosk on startup"
+         3 "Reboot the system appol")
+
+option=$(whiptail --title "Pump Configuration" --menu "Choose an option:" 15 60 4 "${options[@]}" 3>&1 1>&2 2>&3)
+
+if [ $? -eq 0 ]; then
+    case $option in
+        1)
+            sh config/config_splash.sh
+            ;;
+        2)
+
+            sh config/config_kiosk.sh
+            ;;
+        3)
+            sudo reboot
+            ;;
+    esac
+fi
